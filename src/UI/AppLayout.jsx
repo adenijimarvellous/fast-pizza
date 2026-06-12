1a50586a9b3c3c13395a773c9243d266f4dd5245
adenijimarvellous <adenijimarvellous33@gmail.com>
2026-06-12 20:56:03 +0100

    Apply Tailwind styles to layout and UI
    Add basic Tailwind classes across the app: set body background/text classes in index.html; style Header with bg-yellow-500, uppercase and tracking-widest on the brand link; center and style the Home heading and highlight the subtitle with text-yellow-500; update CartOverview container and paragraph with stone-themed background/text and stronger font weight. Also commented out the placeholder welcome paragraph in AppLayout. Note: CartOverview has an extraneous uppercase attribute on the div which may be unintended.

--- src/UI/AppLayout.jsx
+++ src/UI/AppLayout.jsx
@@ -14,11 +14,11 @@ function AppLayout() {
       <Header />
 
       <main>
-        <p>
+        {/* <p>
           Welcome to Fast Pizza Co.! We offer a variety of delicious pizzas that
           you can order online. Browse our menu, add your favorite pizzas to the
           cart, and place your order in just a few clicks!
-        </p>
+        </p> */}
         <Outlet />
       </main>
 
